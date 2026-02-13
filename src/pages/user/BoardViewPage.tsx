b6831733d2420597511daa56f62654e93bc0d17e
FredrickJ <82759673+BladeHailSylas@users.noreply.github.com>
2026-02-13 12:33:50 +0900

    Board View / Update / Delete (#17)

--- src/pages/user/BoardViewPage.tsx
+++ src/pages/user/BoardViewPage.tsx
@@ -3,17 +3,36 @@ import { CommentBlock } from "../../components/board/comment/CommentBlock";
 import { BoardActionBar } from "../../components/board/desc/BoardActionBar";
 import { BoardPostPanel, type BoardPostPanelProps } from "../../components/board/desc/BoardPostPanel";
 import { useNavigate, useParams } from "react-router-dom";
-import axios from "axios";
-
-const token = localStorage.getItem("token");
+import { api } from "../../api/axiosInstance";
 
 export function BoardViewPage() {
     const postId = useParams().postId;
     const navigate = useNavigate();
-    const [post, setPost] = useState<Board>();
+    const [post, setPost] = useState<BoardPostPanelProps>({
+      title: "",
+      writerName: "",
+      boardType: "",
+      createdAt: "",
+      content: "",
+    }); //default value required
+
+    const handleDelete = () => {
+      if (!postId || !confirm("정말로 이 게시글을 삭제하시겠습니까?")) return;
+      api.delete(`/boards/delete/${postId}`).then(() => {
+        alert("게시글이 삭제되었습니다.");
+        navigate("/board");
+      }).catch((err) => {
+        console.error("게시글 삭제 실패: ", err);
+      });
+    }
+
     useEffect(() => {
       api.get(`/boards/list/${postId}`).then((res) => {
         setPost(res.data);
+      }).catch((err) => {
+        console.error("데이터 로드 실패: ", err);
+        alert("게시글 로드 중 오류가 발생했습니다.");
+        navigate("/board");
       });
     }, [postId]);
 
@@ -25,8 +44,8 @@ export function BoardViewPage() {
         <BoardActionBar
           onBack={() => { navigate(-1); }}
           onWrite={() => { navigate('/board/upload') }}
-          onEdit={() => {}}
-          onDelete={handleDelete}
+          onEdit={() => {navigate(`/board/edit/${postId}`)}}
+          onDelete={() => {handleDelete();}}
         />
       </div>
 
