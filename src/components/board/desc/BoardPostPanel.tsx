0f0847f65de5a367e7b048ea30a85be9266ab3ba
FredrickJ <jym352857@gmail.com>
2026-02-13 12:33:21 +0900

    Board View / Update / Delete

--- src/components/board/desc/BoardPostPanel.tsx
+++ src/components/board/desc/BoardPostPanel.tsx
@@ -1,3 +1,5 @@
+import displayCategory from "../../../utils/displayCategory";
+
 export interface BoardPostPanelProps {
   title: string;
   writerName: string;
@@ -17,19 +19,19 @@ export function BoardPostPanel({
     <section className="rounded-lg space-y-4">
       {/* Header */}
       <div className="space-y-1">
-        <h1 className="text-xl font-semibold">{post.title}</h1>
+        <h1 className="text-xl font-semibold">{title}</h1>
         <div className="text-sm text-gray-500 flex gap-2">
           <span>{writerName}</span>
           <span>·</span>
-          <span>{boardType}</span>
+          <span>{displayCategory(boardType)}</span>
           <span>·</span>
-          <span>{createdAt}</span>*/}
+          <span>{new Date(createdAt).toLocaleDateString()}</span>
         </div>
       </div>
 
       {/* Content */}
       <div className="pt-4 text-sm text-gray-800 leading-relaxed whitespace-pre-line">
-        {post.content}
+        {content}
       </div>
     </section>
   );
