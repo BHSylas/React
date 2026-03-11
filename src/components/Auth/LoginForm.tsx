1ed3c1af5a59f5e4cbc141048244a3e29426c728
kuh0701 <kuh0701@naver.com>
2026-03-11 10:04:30 +0900

    로그인 시 닉네임 안 뜨는 거 수정

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -47,7 +47,7 @@ export default function LoginForm() {
                 password,
             });
 
-            const { accessToken, name, nickname } = res.data;
+            const { accessToken, name, userNickname: nickname } = res.data;
 
             login({
                 token: accessToken,
